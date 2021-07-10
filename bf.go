986e0d9a71feaeeb693ce75790ac3e74cdac0cba
Metalblueberry <metalblueberry@gmail.com>
2021-07-10 12:18:25 +0200

    encoding simple

--- bf.go
+++ bf.go
@@ -25,7 +25,9 @@ package main
 import (
 	"bufio"
 	"errors"
+	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 )
@@ -87,40 +89,59 @@ func compile_bf(input string) (program []Instruction, err error) {
 	return
 }
 
-func execute_bf(program []Instruction) {
+type Executor struct {
+	MaxIterations int
+}
+
+func (e *Executor) Execute(program []Instruction) error {
 	data := make([]int16, data_size)
-	var data_ptr uint16 = 0
+	var ptr uint16 = uint16(data_size) / 2
 	reader := bufio.NewReader(os.Stdin)
+	steps := 0
 	for pc := 0; pc < len(program); pc++ {
+		steps++
+		if steps > e.MaxIterations {
+			return errors.New("Max iterations reached")
+		}
 		switch program[pc].operator {
 		case op_inc_dp:
-			data_ptr++
+			ptr++
 		case op_dec_dp:
-			data_ptr--
+			ptr--
 		case op_inc_val:
-			data[data_ptr]++
+			data[ptr]++
 		case op_dec_val:
-			data[data_ptr]--
+			data[ptr]--
 		case op_out:
-			fmt.Printf("%c", data[data_ptr])
+			fmt.Printf("%c - %d\n", data[ptr], data[ptr])
 		case op_in:
-			read_val, _ := reader.ReadByte()
-			data[data_ptr] = int16(read_val)
+			readVal, err := reader.ReadByte()
+			switch {
+			case err == io.EOF:
+				data[ptr] = int16(0)
+			case err != nil:
+				return fmt.Errorf("Unable to read input, %w", err)
+			default:
+				data[ptr] = int16(readVal)
+			}
 		case op_jmp_fwd:
-			if data[data_ptr] == 0 {
+			if data[ptr] == 0 {
 				pc = int(program[pc].operand)
 			}
 		case op_jmp_bck:
-			if data[data_ptr] > 0 {
+			if data[ptr] > 0 {
 				pc = int(program[pc].operand)
 			}
 		default:
 			panic("Unknown operator.")
 		}
 	}
+	return nil
 }
 
 func main() {
+	maxSteps := flag.Int("max-steps", 10000, "limit the number of interations")
+
 	args := os.Args
 	if len(args) != 2 {
 		fmt.Printf("Usage: %s filename\n", args[0])
@@ -137,5 +158,12 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	execute_bf(program)
+	exe := Executor{
+		MaxIterations: *maxSteps,
+	}
+	fmt.Printf("Starting\n")
+	err = exe.Execute(program)
+	if err != nil {
+		fmt.Print(err)
+	}
 }
